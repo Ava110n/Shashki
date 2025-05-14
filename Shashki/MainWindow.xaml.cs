3d18e3889db5cbbde470047d539bb093ee20c0ec
RuVMakarov <RuVMakarov@IMM-14-509-1M>
2025-05-14 12:43:27 +0300

    Добавьте файлы проекта.

---
+++ Shashki/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shashki;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    int[,] shashki = new int[8,8];
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        var canvas = new Canvas()
+        {
+            Name = "canvas",
+            Width = 800,
+            Height = 800
+        };
+        this.AddChild(canvas);
+
+        var points = new PointCollection();
+        points.Add(new Point(0, 0));
+        points.Add(new Point(800, 0));
+        points.Add(new Point(800, 800));
+        points.Add(new Point(0, 800));
+
+        var polygon = new Polygon()
+        {
+            Fill = Brushes.Black,
+            Points = points,
+            Stroke = Brushes.Black,
+            StrokeThickness = 5
+        };
+        canvas.Children.Add(polygon);
+
+        for(int i =0; i<8; i++)
+        {
+            for(int j=0; j<8; j++)
+            {
+                if ((i + j) % 2 == 0)
+                {
+                    var square = new Rectangle()
+                    {
+                        Fill = Brushes.White,//new SolidColorBrush(Color.FromRgb(0, 0, 0))
+                        Width = canvas.Width / 8,
+                        Height = canvas.Height / 8,
+                        Margin = new Thickness(i*canvas.Width/8, j*canvas.Height/8, 0, 0),
+                    };
+                    canvas.Children.Add(square);
+                }
+            }
+        }
+
+
+        
+    }
+}
