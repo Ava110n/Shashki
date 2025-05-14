00ffb6ba5f490c911021cd831147d5354743e91d
RuVMakarov <RuVMakarov@IMM-14-509-1M>
2025-05-14 13:20:09 +0300

    add functions

--- Shashki/MainWindow.xaml.cs
+++ Shashki/MainWindow.xaml.cs
@@ -29,6 +29,14 @@ public partial class MainWindow : Window
         };
         this.AddChild(canvas);
 
+        drawBorder(canvas);//рисуется черный квадрат - для границы и черных клеток
+        drawCell(canvas);//рисуются белые клетки 
+        
+        startPosition();//заполняем матрицу shaski
+    }
+
+    void drawBorder(Canvas canvas)
+    {
         var points = new PointCollection();
         points.Add(new Point(0, 0));
         points.Add(new Point(800, 0));
@@ -42,11 +50,13 @@ public partial class MainWindow : Window
             Stroke = Brushes.Black,
             StrokeThickness = 5
         };
-        canvas.Children.Add(polygon);
-
-        for(int i =0; i<8; i++)
+        canvas.Children.Add(polygon);        
+    }
+    void drawCell(Canvas canvas)
+    {
+        for (int i = 0; i < 8; i++)
         {
-            for(int j=0; j<8; j++)
+            for (int j = 0; j < 8; j++)
             {
                 if ((i + j) % 2 == 0)
                 {
@@ -55,14 +65,31 @@ public partial class MainWindow : Window
                         Fill = Brushes.White,//new SolidColorBrush(Color.FromRgb(0, 0, 0))
                         Width = canvas.Width / 8,
                         Height = canvas.Height / 8,
-                        Margin = new Thickness(i*canvas.Width/8, j*canvas.Height/8, 0, 0),
+                        Margin = new Thickness(i * canvas.Width / 8, j * canvas.Height / 8, 0, 0),
                     };
                     canvas.Children.Add(square);
                 }
             }
         }
+    }
 
+    void drawStartShaski(Canvas canvas)
+    {
 
-        
+    }
+    void startPosition()
+    {
+        for(int i=0; i<8; i++)
+        {
+            for(int j=0; j<8; j++)
+            {
+                if ((i + j) % 2 == 1)//только черные клетки
+                {
+                    if (i < 3) shashki[i, j] = 1;
+                    if (i > 5) shashki[i, j] = -1;
+                }
+                
+            }
+        }
     }
 }
